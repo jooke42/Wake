03897279cb70b356a7ad831c8b462ac3e9a6bcc6
Alexandre Marteaux <ialexandre.marteaux@gmail.com>
2014-12-12 00:23:59 +0100

    add param db file

---
+++ Assets/include/params_connexion.php
@@ -0,0 +1,16 @@
+
+<?php 
+if (! defined ('TEST_INCLUDE'))
+   die ("Vous ne pouvez pas accéder directement à ce fichier");
+/*$dns="mysql:host=database-etudiants.iut.univ-paris8.fr;dbname=dutinfopw20148;";
+$user="dutinfopw20148";
+$password="upuryzyz";
+*/
+
+
+$dns="mysql:host=sql2.olympe.in;dbname=au5d5sxv;";
+$user="au5d5sxv";
+$password="wakeprojetiut";
+
+
+?>
